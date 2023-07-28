5b6f7cd8c8c8af295547caaa4f582c9110766bc6
Tobias Fenster <tfenster@4psbau.de>
2023-07-28 14:22:07 +0000

    add cluster create

---
+++ deploy-cluster.sh
@@ -0,0 +1,18 @@
+# login
+az login
+az account set --subscription="94670b10-08d0-4d17-bcfe-e01f701be9ff"
+
+# create cluster
+location=northeurope
+rg=aks-repro
+clustername=aks-repro
+windowsuser=vm-admin
+windowspassword=Passw0rd*123
+
+az group create --name $rg --location $location
+az aks create --resource-group $rg --name $clustername --node-count 1 --enable-addons monitoring --generate-ssh-keys --windows-admin-username $windowsuser --windows-admin-password $windowspassword --vm-set-type VirtualMachineScaleSets --network-plugin azure
+az aks get-credentials --resource-group $rg --name $clustername
+az aks nodepool add --resource-group $rg --cluster-name $clustername --os-type Windows --os-sku Windows2022 --name npwin --node-count 1
+
+# delete resource group
+az group delete --name $rg --yes --no-wait
