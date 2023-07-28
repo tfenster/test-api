e1e3c16d25ace092476eb99d29f63b40d90dd6ba
Tobias Fenster <tfenster@4psbau.de>
2023-07-28 15:47:54 +0000

    fix

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,5 +1,5 @@
 # login
-az login
+az login --use-device-code
 az account set --subscription="94670b10-08d0-4d17-bcfe-e01f701be9ff"
 
 # create cluster
@@ -7,7 +7,7 @@ location=northeurope
 rg=aks-repro
 clustername=aks-repro
 windowsuser=vm-admin
-windowspassword=Passw0rd*123
+windowspassword=Passw0rd*12345
 
 az group create --name $rg --location $location
 az aks create --resource-group $rg --name $clustername --node-count 1 --enable-addons monitoring --generate-ssh-keys --windows-admin-username $windowsuser --windows-admin-password $windowspassword --vm-set-type VirtualMachineScaleSets --network-plugin azure
@@ -15,4 +15,4 @@ az aks get-credentials --resource-group $rg --name $clustername
 az aks nodepool add --resource-group $rg --cluster-name $clustername --os-type Windows --os-sku Windows2022 --name npwin --node-count 1
 
 # delete resource group
-az group delete --name $rg --yes --no-wait
+#az group delete --name $rg --yes --no-wait
