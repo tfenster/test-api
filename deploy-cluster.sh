d793318a404607957a7e9b9b483baedfe237090c
Tobias Fenster <tfenster@4psbau.de>
2023-09-29 06:22:18 +0000

    use 5 nodes

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,5 +1,5 @@
 # login
-az login --use-device-code
+#az login --use-device-code
 az account set --subscription="94670b10-08d0-4d17-bcfe-e01f701be9ff"
 
 # create cluster
@@ -10,9 +10,9 @@ windowsuser=vm-admin
 windowspassword=Passw0rd*12345
 
 az group create --name $rg --location $location
-az aks create --resource-group $rg --name $clustername --node-count 1 --enable-addons monitoring --generate-ssh-keys --windows-admin-username $windowsuser --windows-admin-password $windowspassword --vm-set-type VirtualMachineScaleSets --network-plugin azure
+az aks create --resource-group $rg --name $clustername --node-count 5 --enable-addons monitoring --generate-ssh-keys --windows-admin-username $windowsuser --windows-admin-password $windowspassword --vm-set-type VirtualMachineScaleSets --network-plugin azure
 az aks get-credentials --resource-group $rg --name $clustername
-az aks nodepool add --resource-group $rg --cluster-name $clustername --os-type Windows --os-sku Windows2022 --name npwin --node-count 1
+az aks nodepool add --resource-group $rg --cluster-name $clustername --os-type Windows --os-sku Windows2022 --name npwin --node-count 5
 
 # delete resource group
 #az group delete --name $rg --yes --no-wait
