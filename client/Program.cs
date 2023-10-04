e08ad96d1bebb36301d8d95fe7147fc8ad1e7b28
Tobias Fenster <tfenster@4psbau.de>
2023-10-04 23:24:42 +0000

    handle errors

--- client/Program.cs
+++ client/Program.cs
@@ -10,22 +10,29 @@
         var url = $"http://{ipAddress}/timeout?durationInSeconds={durationInSeconds}&healthfailStartInSeconds={healtfailStartInSeconds}&healthfailDurationInSeconds={healthfailDurationInSeconds}";
 
         Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Invoking {url}");
-        using var client = new HttpClient();
-        client.Timeout = TimeSpan.FromSeconds(durationInSeconds + 10);
-        var response = await client.GetAsync(url);
-        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Result: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
+        try
+        {
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(durationInSeconds + 10);
+            var response = await client.GetAsync(url);
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Result: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Exception: {ex.Message}");
+        }
     }
 
     private static async Task Main(string[] args)
     {
-        var ipAddress = "20.67.132.184";
+        var ipAddress = "20.166.137.178";
         var backend = "Linux";
         await CallBackend(ipAddress, 4, 1, 2, backend);
         await CallBackend(ipAddress, 8, 2, 4, backend);
         await CallBackend(ipAddress, 12, 3, 6, backend);
         await CallBackend(ipAddress, 16, 4, 8, backend);
 
-        ipAddress = "20.82.173.109";
+        ipAddress = "20.166.138.22";
         backend = "Windows";
         await CallBackend(ipAddress, 4, 1, 2, backend);
         await CallBackend(ipAddress, 8, 2, 4, backend);
